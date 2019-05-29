96d726dac16b1ef3fdfe178fbff2fbdbbbfea4c5
Riku Pepponen <riku.pepponen@gmail.com>
2019-05-29 16:53:23 +0300

    Add a CamRotator.cs script

---
+++ SuperHexagon/Assets/Scripts/CamRotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotator : MonoBehaviour
+{
+
+    public float rotationSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed);
+    }
+}
